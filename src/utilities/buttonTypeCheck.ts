1f0abd73cafe674ca29d2f1bbcb1f07fcdc3b936
Dima Savchenko <kolibri380@gmail.com>
2024-01-08 15:07:22 +0100

    minor refactor

--- src/utilities/buttonTypeCheck.ts
+++ src/utilities/buttonTypeCheck.ts
@@ -1,32 +1,9 @@
+export const primaryButtonStyle =
+	"m-[5px] bg-[#fbeee0] border-2 border-[#422800] border-solid rounded-[30px] shadow-[2.5px_2.5px_2px_0_#422800] text-[#422800] cursor-pointer inline-block font-[600] text-[18px] p-[0_18px] leading-[50px] text-center touch-manipulation hover:bg-[#fff] active:shadow-[2px_2px_0_0_#422800] active:transform-[2px_2px] md:min-w-[60px] md:p-[0_10px] md:max-w-[100px]";
+
 export const ButtonTypeCheck = (value: string | null) => {
-	const primaryButtonStyle = `
-                m-[5px]
-                bg-[#fbeee0] 
-                border-2
-                 border-[#422800] 
-                 border-solid
-                  rounded-[30px]
-                   shadow-[2.5px_2.5px_2px_0_#422800]
-                    text-[#422800]
-                     cursor-pointer 
-                     inline-block
-                      font-[600]
-                       text-[18px]
-                        p-[0_18px]
-                         leading-[50px]
-                          text-center 
-                          touch-manipulation
-                          hover:bg-[#fff]
-                          active:shadow-[2px_2px_0_0_#422800]
-                          active:transform-[2px_2px]
-                          md:min-w-[60px]
-                          md:p-[0_10px]
-                          md:max-w-[100px]
-                          `;
 	if (value === "=") {
-		return (
-			primaryButtonStyle + " text-[black] bg-yellow-500 hover:text-[black]"
-		);
+		return primaryButtonStyle + "text-[black] bg-yellow-500 hover:text-[black]";
 	}
 	if (value === "C") {
 		return (
@@ -34,5 +11,5 @@ export const ButtonTypeCheck = (value: string | null) => {
 			"text-[black] bg-red-900 hover:bg-red-700 hover:text-[white]"
 		);
 	}
-	return primaryButtonStyle + "bg-yellow-500 hover:text-[black]";
+	return primaryButtonStyle + " bg-yellow-500 hover:text-[black]";
 };
